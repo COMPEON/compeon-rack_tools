9334845c334bb32f426412dde0c7d48755cf21bc
Gerrit Seger <gseger@compeon.de>
2019-05-13 14:18:26 +0200

    Add temporary Token implementation

--- lib/compeon/rack_tools/pipes/parse_oauth2_authorization_code.rb
+++ lib/compeon/rack_tools/pipes/parse_oauth2_authorization_code.rb
@@ -2,15 +2,16 @@
 
 require 'compeon/rack_tools/http_errors'
 require 'compeon/rack_tools/pipes/log'
+require 'compeon/rack_tools/token'
 
 module Compeon
   module RackTools
     module Pipes
       PARSE_OAUTH2_AUTHORIZATION_CODE = lambda do |code:, **rest|
-        token = Compeon::AccessToken.parse(code)
+        token = Compeon::RackTools::Token.parse_authorization_token(code)
 
         { token: token, **rest }
-      rescue Compeon::AccessToken::ParseError
+      rescue Compeon::RackTools::Token::ParseError => e
         raise Compeon::RackTools::UnprocessableEntityError
       end
     end
