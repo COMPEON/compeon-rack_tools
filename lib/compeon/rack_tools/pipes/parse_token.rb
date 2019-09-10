01ac5bd6af85c54e823df7a333a53530d0015b13
Gerrit Seger <gseger@compeon.de>
2019-09-10 10:28:02 +0200

    Integrate PARSE_TOKEN_WITH_KEY into PARSE_TOKEN

--- lib/compeon/rack_tools/pipes/parse_token.rb
+++ lib/compeon/rack_tools/pipes/parse_token.rb
@@ -1,24 +1,31 @@
 # frozen_string_literal: true
 
 require 'compeon/rack_tools/http_errors'
+require 'compeon/token'
 
 module Compeon
   module RackTools
     module Pipes
-      PARSE_TOKEN = lambda do |request:, **rest|
-        token_string = request.env['HTTP_AUTHORIZATION']&.match(/^token (?<token>.*)/)&.named_captures&.[]('token')
+      PARSE_TOKEN = lambda do |key: nil|
+        lambda do |request:, **rest|
+          token_string = request.env['HTTP_AUTHORIZATION']&.match(/^token (?<token>.*)/)&.named_captures&.[]('token')
 
-        raise Compeon::RackTools::UnauthorizedError unless token_string
+          raise Compeon::RackTools::UnauthorizedError unless token_string
 
-        token = Compeon::RackTools::Token.parse_access_token(token_string)
+          token = if key.nil?
+                    Compeon::RackTools::Token.parse_access_token(token_string)
+                  else
+                    Compeon::Token::Access.decode(encoded_token: token_string, key: key)
+                  end
 
-        {
-          token: token,
-          request: request,
-          **rest
-        }
-      rescue Compeon::RackTools::Token::ParseError
-        raise Compeon::RackTools::UnauthorizedError
+          {
+            token: token,
+            request: request,
+            **rest
+          }
+        rescue Compeon::RackTools::Token::ParseError, Compeon::Token::DecodeError
+          raise Compeon::RackTools::UnauthorizedError
+        end
       end
     end
   end
--- test/compeon/rack_tools/pipes/parse_token_test.rb
+++ test/compeon/rack_tools/pipes/parse_token_test.rb
@@ -19,9 +19,10 @@ module Compeon
         end
 
         def test_parse_token_ok
+          parser = Compeon::RackTools::Pipes::PARSE_TOKEN.call
           token = JWT.encode({ cid: 'client-id', knd: 'access', role: 'role', uid: 'user-id' }, AUTH_KEY, 'RS256')
           request = Rack::Request.new('HTTP_AUTHORIZATION' => "token #{token}")
-          result = Compeon::RackTools::Pipes::PARSE_TOKEN.call(request: request)
+          result = parser.call(request: request)
 
           assert_equal(request, result[:request])
 
@@ -33,16 +34,49 @@ module Compeon
         end
 
         def test_parse_no_token
+          parser = Compeon::RackTools::Pipes::PARSE_TOKEN.call
           assert_raises Compeon::RackTools::UnauthorizedError do
-            Compeon::RackTools::Pipes::PARSE_TOKEN.call(request: Rack::Request.new({}))
+            parser.call(request: Rack::Request.new({}))
           end
         end
 
         def test_parse_gibberish_token
-          request = Rack::Request.new({ 'HTTP_AUTHORIZATION' => 'token ulf' })
+          parser = Compeon::RackTools::Pipes::PARSE_TOKEN.call
+          request = Rack::Request.new('HTTP_AUTHORIZATION' => 'token ulf')
 
           assert_raises Compeon::RackTools::UnauthorizedError do
-            Compeon::RackTools::Pipes::PARSE_TOKEN.call(request: request)
+            parser.call(request: request)
+          end
+        end
+
+        def test_parse_token_with_key_ok
+          parser = Compeon::RackTools::Pipes::PARSE_TOKEN.call(key: AUTH_KEY.public_key)
+          token = JWT.encode({ cid: 'client-id', knd: 'access', role: 'role', uid: 'user-id' }, AUTH_KEY, 'RS256')
+          request = Rack::Request.new('HTTP_AUTHORIZATION' => "token #{token}")
+          result = parser.call(request: request)
+
+          assert_equal(request, result[:request])
+
+          token = result[:token]
+
+          assert_equal('client-id', token.client_id)
+          assert_equal('role', token.role)
+          assert_equal('user-id', token.user_id)
+        end
+
+        def test_parse_no_token_with_key
+          parser = Compeon::RackTools::Pipes::PARSE_TOKEN.call(key: AUTH_KEY.public_key)
+          assert_raises Compeon::RackTools::UnauthorizedError do
+            parser.call(request: Rack::Request.new({}))
+          end
+        end
+
+        def test_parse_gibberish_token_with_key
+          parser = Compeon::RackTools::Pipes::PARSE_TOKEN.call(key: AUTH_KEY.public_key)
+          request = Rack::Request.new('HTTP_AUTHORIZATION' => 'token ulf')
+
+          assert_raises Compeon::RackTools::UnauthorizedError do
+            parser.call(request: request)
           end
         end
       end
