3fff003a8fab47716265c680f8e95103c4647f72
Gerrit Seger <gseger@compeon.de>
2019-09-10 12:18:01 +0200

    Use new authorization token class for PARSE_OAUTH2_AUTHORIZATION_CODE pipe

--- test/compeon/rack_tools/pipes/parse_oauth2_authorization_code_test.rb
+++ test/compeon/rack_tools/pipes/parse_oauth2_authorization_code_test.rb
@@ -10,18 +10,14 @@ module Compeon
       class ParseOauth2AuthorizationCodeTest < Minitest::Test
         AUTH_KEY = OpenSSL::PKey::RSA.new(512)
 
-        def setup
-          Compeon::RackTools::Token.public_key = AUTH_KEY.public_key
-        end
-
-        def teardown
-          Compeon::RackTools::Token.public_key = nil
+        def parser
+          Compeon::RackTools::Pipes::PARSE_OAUTH2_AUTHORIZATION_CODE.call(key: AUTH_KEY.public_key)
         end
 
         def test_with_a_valid_code
-          code = JWT.encode({ cid: 'client-id', knd: 'auth', uid: 'user-id' }, AUTH_KEY, 'RS256')
+          code = JWT.encode({ cid: 'client-id', knd: 'auth', uid: 'user-id', uri: 'uri' }, AUTH_KEY, 'RS256')
 
-          result = Compeon::RackTools::Pipes::PARSE_OAUTH2_AUTHORIZATION_CODE.call(code: code, other_parameter: :stub)
+          result = parser.call(code: code, other_parameter: :stub)
           token = result[:auth_token]
 
           assert_equal('client-id', token.client_id)
@@ -32,7 +28,13 @@ module Compeon
 
         def test_with_and_invalid_code
           assert_raises Compeon::RackTools::UnprocessableEntityError do
-            Compeon::RackTools::Pipes::PARSE_OAUTH2_AUTHORIZATION_CODE.call(code: 'invlid code')
+            parser.call(code: 'invlid code')
+          end
+        end
+
+        def test_parse_no_key
+          assert_raises 'Invalid key of class `NilClass` given.' do
+            Compeon::RackTools::Pipes::PARSE_OAUTH2_AUTHORIZATION_CODE.call(key: nil)
           end
         end
       end
